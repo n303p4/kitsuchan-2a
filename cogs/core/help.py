5d2076d8f1994f364ddf5d72b85d0b8e19a2e715
Anurag <ctrldelme@gmail.com>
2018-03-16 07:32:40 +0530

    added the new dynamic help command
    also for docstrings, formatting wont work so for examples and similar stuff when you need ctx.prefix, you can just use %prefix%

--- cogs/core/help.py
+++ cogs/core/help.py
@@ -1,18 +1,18 @@
-#!/usr/bin/env python3
-
+import discord
 from discord.ext import commands
+import itertools
 
 
 class Help:
     """Help command."""
 
+    def __init__(self, bot):
+        self.bot = bot
+   
     @commands.command(aliases=["commands"])
     @commands.cooldown(1, 2)
-    async def help(self, ctx, *cmds: str):
-        """Help command.
-
-        * command_or_cog - The name of a command or cog.
-        """
+    async def help(self, ctx, *, cmds: str=None):
+        """help command for the bot"""
         if not cmds:
             commands_list = []
             for command in ctx.bot.commands:
@@ -25,17 +25,45 @@ class Help:
                 if can_run:
                     commands_list.append(command.name)
             commands_list.sort()
-            help_text = f'```{", ".join(commands_list)}```'
-            help_text += f"\nRun **help command** for more details on a command."
-            help_text = "**List of commands:**\n" + help_text
-            await ctx.send(help_text)
+            help_text = f'{", ".join(f"`{cmd}`" for cmd in commands_list)}'
+
+            embed = discord.Embed(title="Commands:",
+                                  description=f"{help_text}")
+            embed.set_footer(
+                text=f"Use `{ctx.prefix}help <command>` for more info on a command.", icon_url=ctx.author.avatar_url_as(format="png"))
+
+            embed.add_field(name="For more help join the support server.",
+                            value="https://discord.gg/EeqMvs")
+
+            await ctx.send(embed=embed)
+
         else:
-            # This is awful, haha
-            await ctx.bot.all_commands["old_help"].callback(ctx, *cmds)
+            embed = discord.Embed()
+            embed.set_footer(
+                text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar_url_as(format="png"))
+            try:
+                sub = self.bot.get_command(cmds).commands
+                if not sub:
+                    return await ctx.send("Command not found")
+                else:
+                    for cmd in sub:
+                        embed.add_field(
+                            name=f"**{ctx.prefix}{cmd.signature}**", value=cmd.help.replace("%prefix%", ctx.prefix))
+            except Exception:
+                command = self.bot.get_command(cmds)
+                if command.help:
+                    helptxt = command.help
+                else:
+                    helptxt = "Help not given."
+
+                embed.add_field(
+                    name=f"**{ctx.prefix}{command.signature}**", value=helptxt.replace("%prefix%", ctx.prefix))
+            await ctx.send(embed=embed)
 
 
 def setup(bot):
     """Set up the extension."""
+    #following code is taken from Kitsuchan 2 by Foxo
     try:
         help_command = bot.all_commands["help"]
         help_command.hidden = True
@@ -44,4 +72,5 @@ def setup(bot):
         bot.add_command(help_command)
     except KeyError:  # This means the setup already did its thing.
         pass
-    bot.add_cog(Help())
+    bot.add_cog(Help(bot))
+
